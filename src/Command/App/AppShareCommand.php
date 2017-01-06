cf3c39936888c9688b445d6a94546632a4b58a13
Arek van Schaijk <info@ucreation.nl>
2017-01-06 16:31:45 +0100

    Feature extended app:share with an option to select the room on HipChat

--- src/Command/App/AppShareCommand.php
+++ src/Command/App/AppShareCommand.php
@@ -10,6 +10,7 @@ use AlterNET\Cli\Utility\StringUtility;
 use GorkaLaucirica\HipchatAPIv2Client\Model\Message;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -34,6 +35,7 @@ class AppShareCommand extends CommandBase
         $this->setName('app:share');
         $this->setDescription('Share things about the application on HipChat');
         $this->addArgument('subject', InputArgument::OPTIONAL, 'Relative file path or an option between brackets ([..])');
+        $this->addOption('room', null, InputOption::VALUE_REQUIRED, 'The HipChat room (room id or room name)');
     }
 
     /**
@@ -85,8 +87,10 @@ class AppShareCommand extends CommandBase
         $this->preventNotBeingInAnApp();
         // This loads the app where we are in (working directory)
         $app = AppUtility::load();
+        // Gets the room id
+        $roomId = $this->collectHipChatRoomId($app);
         // This checks if the app has a HipChat integration (if the room id is known)
-        if (!$app->getConfig()->getHipChatRoomId()) {
+        if (!$roomId) {
             $this->io->error('The application has no HipChat configuration.');
             exit;
         }
@@ -109,7 +113,7 @@ class AppShareCommand extends CommandBase
             $method = $categories[$category]['method'];
             // Executes the category method
             if (method_exists($this, $method)) {
-                $this->$method($app);
+                $this->$method($app, $roomId);
             } else {
                 throw new Exception('The method belonging to the category [' . $category . '] does not exists.');
             }
@@ -119,28 +123,63 @@ class AppShareCommand extends CommandBase
             if (StringUtility::isAbsolutePath($subject)) {
                 $filePath = $app->getWorkingDirectory() . $subject;
             }
-            $this->hipChatFileContent($filePath, $app->getConfig()->getHipChatRoomId());
+            $this->hipChatFileContent($filePath, $roomId);
         }
+    }
+
+    /**
+     * Collects the HipChat RoomId
+     *
+     * @param App $app
+     * @return int|null
+     */
+    protected function collectHipChatRoomId(App $app)
+    {
+        $roomId = ($app->getConfig()->getHipChatRoomId() ?: null);
+        if (($search = $this->input->getOption('room'))) {
+            if (ctype_digit($search)) {
+                $roomId = $this->hipChatDriver()->getRoom($search)->getId();
+            } else {
+                $rooms = $this->hipChatDriver()->getRooms($search);
+                if ($rooms) {
+                    if (count($rooms) === 1) {
+                        $roomId = $rooms[0]->getId();
+                    } else {
+                        $choices = [];
+                        foreach ($rooms as $room) {
+                            $choices[$room->getName()] = $room->getId();
+                        }
+                        $roomId = $this->io->choice('Select the room where you would like to share', $choices);
+                    }
+                } else {
+                    $this->io->error('Could not find any room with "' . $search . '".');
+                    exit;
+                }
+            }
+        }
+        return $roomId;
     }
 
     /**
      * Executes the Category Config
      *
      * @param App $app
+     * @param int $roomId
      * @return void
      */
-    protected function executeCategoryConfig(App $app)
+    protected function executeCategoryConfig(App $app, $roomId)
     {
-        $this->hipChatFileContent($app->getConfigFilePath(), $app->getConfig()->getHipChatRoomId());
+        $this->hipChatFileContent($app->getConfigFilePath(), $roomId);
     }
 
     /**
      * Executes the Important Message
      *
      * @param App $app
+     * @param int $roomId
      * @return void
      */
-    protected function executeCategoryImportantMessage(App $app)
+    protected function executeCategoryImportantMessage(App $app, $roomId)
     {
         if (($setMessage = $this->io->ask('Set the message'))) {
             // Makes a new HipChat message
@@ -150,7 +189,7 @@ class AppShareCommand extends CommandBase
             $message->setNotify(true);
             $message->setFrom('Important message');
             // And sends it
-            $this->hipChatDriver()->sendMessage($message, $app->getConfig()->getHipChatRoomId());
+            $this->hipChatDriver()->sendMessage($message, $roomId);
             $this->io->success('The message was sent successfully.');
         }
     }
